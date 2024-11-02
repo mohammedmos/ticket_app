2596c5a78731b7f4d847cfa046aa3d17a769ffd7
Mohammed Haida Mosbah <lampmohammed07@gmail.com>
2024-11-02 18:21:08 +0100

    chore: app bar

--- lib/screens/search/widgets/ticket_tabs.dart
+++ lib/screens/search/widgets/ticket_tabs.dart
@@ -11,28 +11,33 @@ class TicketTabs extends StatelessWidget {
         borderRadius: BorderRadius.circular(50),
         color: const Color(0xFFF4F6FD)
       ),
-      child: Row(
+      child: const Row(
         children: [
-           Container(
-            padding: const EdgeInsets.symmetric(vertical: 7),
-            width: size.width * .44,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.horizontal(left: Radius.circular(50))
-            ),
-            child: const Center(child: Text('Airline Tickets')),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 7),
-            width: size.width * .44,
-            decoration: const BoxDecoration(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.horizontal(left: Radius.circular(50))
-            ),
-            child: const Center(child: Text('Hotels')),
-          )
+          AppTab(tabText: "Airline Tickets"),
+          AppTab(tabText: "Hotels",tabBorder: true,tabColor: true),
         ],
       ),
     );
   }
 }
+class AppTab extends StatelessWidget {
+  const AppTab({super.key, this.tabText = "", this.tabBorder = false,  this.tabColor=false});
+
+  final String tabText;
+  final bool tabBorder;
+  final bool tabColor;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return  Container(
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      width: size.width * .44,
+      decoration:  BoxDecoration(
+          color: tabColor ==false? Colors.white:Colors.transparent,
+          borderRadius: tabBorder == false? const BorderRadius.horizontal(left: Radius.circular(50)): const BorderRadius.horizontal(right: Radius.circular(50))
+      ),
+      child:  Center(child: Text(tabText)),
+    );
+  }
+}
+
